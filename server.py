17a69a3e05bb5347282cc9926c7f818b41f79a13
Saladay Aleksey <saladay@yandex.ru>
2024-03-18 12:16:02 +0500

    all

---
+++ server.py
@@ -0,0 +1,17 @@
+from flask import Flask
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
+
+
+def main():
+    app.run()
+
+
+@app.route("/")
+def index():
+    return 'ok'
+
+
+if __name__ == '__main__':
+    main()
